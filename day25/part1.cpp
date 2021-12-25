e7c37c59db15d7ebcc5a2a81128ef05f8c206448
Neno Ganchev <neno.ganchev@receipt-bank.com>
2021-12-26 00:06:26 +0200

    wip: Add solutions for day 25

---
+++ day25/part1.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class seafloor_map {
+public:
+    enum tile_type {east, south, empty};
+
+    seafloor_map(const string &filename);
+    seafloor_map(size_t size_x, size_t size_y);
+
+    bool operator == (const seafloor_map &rhs) const = default;
+
+    size_t size_x() const;
+    size_t size_y() const;
+    tile_type at(size_t x, size_t y) const;
+    void set(size_t x, size_t y, tile_type tile);
+    void print() const;
+
+private:
+    vector<tile_type> buffer;
+    size_t _size_x;
+    size_t _size_y;
+};
+
+int steps_to_equilibrium(seafloor_map &map);
+seafloor_map simulate_step(const seafloor_map &map);
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cout << "Usage: <exe> <filename>\n";
+        return 1;
+    }
+    const string filename = argv[1];
+
+    seafloor_map map(filename);
+
+    cout << "Initial map:" << endl;
+    map.print();
+
+    cout << "Equilibrium reached after: " << steps_to_equilibrium(map) << " steps" << endl;
+}
+
+seafloor_map::seafloor_map(const string &filename) {
+    ifstream input_file(filename);
+
+    this->_size_x = this->_size_y = 0;
+    size_t map_width = 0;
+
+    int c;
+    while ((c = input_file.get()) != ifstream::traits_type::eof()) {
+        tile_type tile;
+        switch (c) {
+        case '>':
+            tile = east;
+            break;
+
+        case 'v':
+            tile = south;
+            break;
+
+        case '.':
+            tile = empty;
+            break;
+
+        case '\n':
+            this->_size_x = map_width;
+            ++this->_size_y;
+            map_width = 0;
+            continue;
+
+        default:
+            throw runtime_error("Invalid tile type");
+        }
+
+        this->buffer.push_back(tile);
+        ++map_width;
+    }
+}
+
+seafloor_map::seafloor_map(size_t size_x, size_t size_y):
+buffer(size_x * size_y, empty), _size_x(size_x), _size_y(size_y) {}
+
+size_t seafloor_map::size_x() const {
+    return this->_size_x;
+}
+
+size_t seafloor_map::size_y() const {
+    return this->_size_x;
+}
+
+seafloor_map::tile_type seafloor_map::at(size_t x, size_t y) const {
+    if (x == this->_size_x) x = 0;
+    if (y == this->_size_y) y = 0;
+
+    return this->buffer[y * this->_size_x + x];
+}
+
+void seafloor_map::set(size_t x, size_t y, tile_type tile) {
+    if (x == this->_size_x) x = 0;
+    if (y == this->_size_y) y = 0;
+
+    this->buffer[y * this->_size_x + x] = tile;
+}
+
+void seafloor_map::print() const {
+    auto tile_to_char = [](tile_type tile) {
+        switch (tile) {
+        case east: return '>';
+        case south: return 'v';
+        case empty: return '.';
+        }
+    };
+
+    for (size_t y = 0; y < this->_size_y; ++y) {
+        for (size_t x = 0; x < this->_size_x; ++x) {
+            cout << tile_to_char(this->at(x, y));
+        }
+        cout << endl;
+    }
+}
+
+int steps_to_equilibrium(seafloor_map &map) {
+    int steps = 0;
+    for (;;) {
+        seafloor_map new_map = simulate_step(map);
+        if (new_map == map) return steps;
+
+        ++steps;
+        map = move(new_map);
+    }
+}
+
+seafloor_map simulate_step(const seafloor_map &map) {
+    seafloor_map new_map(map.size_x(), map.size_y());
+
+    for (size_t y = 0; y < map.size_y(); ++y) {
+        for (size_t x = 0; x < map.size_x(); ++x) {
+            if (map.at(x, y) == seafloor_map::east) {
+                size_t new_x_pos = map.at(x + 1, y) == seafloor_map::empty ? x + 1 : x;
+                new_map.set(new_x_pos, y, seafloor_map::east);
+            }
+        }
+    }
+
+    for (size_t y = 0; y < map.size_y(); ++y) {
+        for (size_t x = 0; x < map.size_x(); ++x) {
+            if (map.at(x, y) == seafloor_map::south) {
+                size_t new_y_pos;
+                if (map.at(x, y + 1) == seafloor_map::south || new_map.at(x, y + 1) == seafloor_map::east) {
+                    new_y_pos = y;
+                }
+                else {
+                    new_y_pos = y + 1;
+                }
+                new_map.set(x, new_y_pos, seafloor_map::south);
+            }
+        }
+    }
+
+    return new_map;
+}
